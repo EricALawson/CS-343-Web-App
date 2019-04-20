a4045862fca54d1b2cb0f5d3403cd1abd9b02fb2
Jeremiah St.Pierre <jeremiahstpierre@yahoo.com>
2019-04-20 15:30:28 -0700

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var app = express();
 var path = require('path');
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + 'public/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 //app.use(express.static(__dirname +'/public'));
 app.listen(3000);
