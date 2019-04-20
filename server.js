9dd80eddf86323518fc99d6061057f34aa38d393
Jeremiah St.Pierre <jeremiahstpierre@yahoo.com>
2019-04-20 15:09:57 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
-const http = require('http');
-var express = require('express'); 
-//var fs = require('fs');
+// const http = require('http');
 
-const hostname = '134.209.14.125';
-//const port = 3000;
+// const hostname = '134.209.14.125';
+// //const port = 3000;
 
-/*const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-*/
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
 
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+var express = require('express');
 var app = express();
-app.get('/test', function(req, res) {
-    res.sendFile('views/test.html', {root: __dirname })
+var path = require('path');
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-
-var port = process.env.PORT || 3000;
-var server = app.listen(port);
-
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(3000);
