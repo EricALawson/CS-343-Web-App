49a203fa3536ddc8b5c013455f483ebdf50bf14e
Jon Sohrabi <jon.sohrabi@gmail.com>
2019-04-27 16:23:08 -0700

    Added Login Functionality

---
+++ public/scripts/init-firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+	apiKey: "AIzaSyDZWlHDj1CLih_xRVLGg2JSxTdWTMeGJXU",
+	authDomain: "gameme-6de77.firebaseapp.com",
+	databaseURL: "https://gameme-6de77.firebaseio.com",
+	projectId: "gameme-6de77",
+	storageBucket: "gameme-6de77.appspot.com",
+	messagingSenderId: "1040081357704"
+};
+
+firebase.initializeApp( firebaseConfig );
---
+++ public/scripts/loginFunctions.js
@@ -0,0 +1,36 @@
+document.addEventListener( "DOMContentLoaded", event => {
+	const app = firebase.app();
+});
+
+var googleLogin = () => {
+	const provider = new firebase.auth.GoogleAuthProvider();
+
+	firebase.auth().signInWithPopup( provider )
+		.then( result => {
+			const user = result.user;
+			window.location.href( "http://localhost:3000" );	//	<==== TODO: Change to user's current web page
+			console.log( user );
+		})
+		.catch( console.log )
+}
+
+var googleLogout = () => {
+	firebase.auth().signOut()
+		.then( function() {
+			window.location.href( "http://localhost:3000" );
+		}).catch( console.log )
+}
+
+var isLoggedIn = () => {
+	firebase.auth().onAuthStateChanged( function( user ) {
+		if( user ) {
+			document.getElementById( 'usernameDiv' ).innerHTML = `Logged in as ${user.displayName}`;
+			document.getElementById( 'loginButton' ).innerHTML = `Logout`;
+			document.getElementById( 'loginButton' ).setAttribute( "onClick", "googleLogout()" );
+		} else {
+			document.getElementById( 'usernameDiv' ).innerHTML = `You are currently not logged in`;
+			document.getElementById( 'loginButton' ).innerHTML = `Login`;
+			document.getElementById( 'loginButton' ).setAttribute( "onClick", "googleLogin()" );
+		}
+	});
+}
